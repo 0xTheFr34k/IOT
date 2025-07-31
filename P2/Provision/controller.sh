558d7b3248d4b4c10c4a36e58c2759ab9a6c3ad8
0xTheFr34k <tayayassine6@live.fr>
2025-07-31 17:30:38 +0100

    never forget the --flannel-iface

--- P2/Provision/controller.sh
+++ P2/Provision/controller.sh
@@ -3,8 +3,6 @@ set -e
 
 systemctl disable firewalld --now
 
-SIP=192.168.56.110
-
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=$SIP --advertise-address=$SIP" sh -s - --token ytaya
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=192.168.56.110  --flannel-iface eth1 --token ytaya" sh -s -
 
 cp /usr/local/bin/kubectl /usr/sbin/kubectl
--- P2/Provision/worker.sh
+++ P2/Provision/worker.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-MASTER_IP=192.168.56.110
-WIP=192.168.56.111
-
 systemctl disable firewalld --now
 
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://$MASTER_IP:6443 --token ytaya --node-ip=$WIP" sh -s -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://192.168.56.110:6443 --token ytaya --node-ip=192.168.56.111 --flannel-iface eth1" sh -s -
