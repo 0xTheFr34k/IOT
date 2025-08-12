666a52e26909faee9255184b4c6df3f0118d3b60
0xTheFre34k <tayayassine6@live.fr>
2025-08-12 11:45:54 +0100

    change box for macos

--- P1/Provision/controller.sh
+++ P1/Provision/controller.sh
@@ -3,7 +3,9 @@ set -e
 
 SIP=192.168.56.110
 
-systemctl disable firewalld --now
+# systemctl disable firewalld --now
+
+ufw disable
 
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=$SIP --advertise-address=$SIP" sh -s - --token ytaya
 
--- P1/Provision/worker.sh
+++ P1/Provision/worker.sh
@@ -4,6 +4,8 @@ set -e
 MASTER_IP=192.168.56.110
 WIP=192.168.56.111
 
-systemctl disable firewalld --now
+# systemctl disable firewalld --now
+
+ufw disable
 
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://$MASTER_IP:6443 --token ytaya --node-ip=$WIP" sh -s -
